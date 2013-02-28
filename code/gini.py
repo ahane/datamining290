8aa4deeec738344791893fd07471716fe97f1b62
Alec Hanefeld <Alec Hanefeld>
2013-02-27 17:33:29 -0800

    finished assingment 3

--- code/gini.py
+++ code/gini.py
@@ -5,7 +5,7 @@ Classes are strings."""
 
 import fileinput
 import csv
-
+from collections import Counter, defaultdict
 (cmte_id, cand_id, cand_nm, contbr_nm, contbr_city, contbr_st, contbr_zip,
 contbr_employer, contbr_occupation, contb_receipt_amt, contb_receipt_dt,
 receipt_desc, memo_cd, memo_text, form_tp, file_num, tran_id, election_tp) = range(18)
@@ -13,21 +13,30 @@ receipt_desc, memo_cd, memo_text, form_tp, file_num, tran_id, election_tp) = ran
 
 ############### Set up variables
 # TODO: declare datastructures
-
+zipcodes = defaultdict(Counter)
+candidates = Counter()
 ############### Read through files
 for row in csv.reader(fileinput.input()):
     if not fileinput.isfirstline():
         ###
         # TODO: replace line below with steps to save information to calculate
         # Gini Index
-        row[cand_nm], row[contbr_zip]
-        ##/
+
+        zipcodes[row[contbr_zip]][row[cand_nm]] += 1 
+        candidates[row[cand_nm]] += 1
+##/
 
 ###
 # TODO: calculate the values below:
-gini = 0  # current Gini Index using candidate name as the class
+total = sum(candidates.values())
+gini = 1 - sum(((float(count)/total)**2 for count in candidates.values()))   # current Gini Index using candidate name as the class
+
 split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
+for zipcode in zipcodes.keys():
+    zip_total = sum(zipcodes[zipcode].values())
+    zip_gini = 1 - sum(((float(count)/zip_total)**2 for count in zipcodes[zipcode].values()))
+    split_gini += (float(zip_total)/total)*zip_gini
 
 print "Gini Index: %s" % gini
 print "Gini Index after split: %s" % split_gini
